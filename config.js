0c60958299174669cc97381369340d752e3afaef
Yuri <yuri.belorusets@gmail.com>
2022-04-20 10:57:18 -0700

    did some exploration of app and setup

--- config.js
+++ config.js
@@ -12,11 +12,19 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+    ? "postgres://postgres:xwzpdq@localhost:5432/jobly_test"
+    : `postgres://postgres:xwzpdq@localhost:5432/${process.env.DATABASE_URL}` || "postgres://postgres:xwzpdq@localhost:5432/jobly";
 }
 
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+
+//     function getDatabaseUri() {
+//       return (process.env.NODE_ENV === "test")
+//           ? "jobly_test"
+//           : process.env.DATABASE_URL || "jobly";
+//     }
+
+
+// // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
