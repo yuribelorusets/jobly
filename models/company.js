a1c09d754780bb698744f0bb5f5bc2293f9662fa
Yuri <yuri.belorusets@gmail.com>
2022-04-20 12:15:53 -0700

    worked on filtering the get companies query

--- models/company.js
+++ models/company.js
@@ -18,16 +18,16 @@ class Company {
 
   static async create({ handle, name, description, numEmployees, logoUrl }) {
     const duplicateCheck = await db.query(
-        `SELECT handle
+      `SELECT handle
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]);
 
     if (duplicateCheck.rows[0])
       throw new BadRequestError(`Duplicate company: ${handle}`);
 
     const result = await db.query(
-        `INSERT INTO companies(
+      `INSERT INTO companies(
           handle,
           name,
           description,
@@ -36,13 +36,13 @@ class Company {
            VALUES
              ($1, $2, $3, $4, $5)
            RETURNING handle, name, description, num_employees AS "numEmployees", logo_url AS "logoUrl"`,
-        [
-          handle,
-          name,
-          description,
-          numEmployees,
-          logoUrl,
-        ],
+      [
+        handle,
+        name,
+        description,
+        numEmployees,
+        logoUrl,
+      ],
     );
     const company = result.rows[0];
 
@@ -54,8 +54,9 @@ class Company {
    * Returns [{ handle, name, description, numEmployees, logoUrl }, ...]
    * */
 
-  static async findAll() {
-    const companiesRes = await db.query(
+  static async findAll(filters) {
+    if (!filters) {
+      const companiesRes = await db.query(
         `SELECT handle,
                 name,
                 description,
@@ -63,7 +64,27 @@ class Company {
                 logo_url AS "logoUrl"
            FROM companies
            ORDER BY name`);
-    return companiesRes.rows;
+      return companiesRes.rows;
+    }
+
+    const { setCols, values } = sqlForPartialUpdate(
+      data,
+      {
+        nameLike: "name",
+        minEmployees: "num_employees",
+        maxEmployees: "num_employees"
+      });
+    const handleVarIdx = "$" + (values.length + 1);
+
+    const querySql = `
+    SELECT handle,
+      name,
+      description,
+      num_employees AS "numEmployees",
+      logo_url AS "logoUrl"
+    FROM companies
+    ORDER BY name`;
+    const result = await db.query(querySql, [...values, handle]);
   }
 
   /** Given a company handle, return data about company.
@@ -76,14 +97,14 @@ class Company {
 
   static async get(handle) {
     const companyRes = await db.query(
-        `SELECT handle,
+      `SELECT handle,
                 name,
                 description,
                 num_employees AS "numEmployees",
                 logo_url AS "logoUrl"
            FROM companies
            WHERE handle = $1`,
-        [handle]);
+      [handle]);
 
     const company = companyRes.rows[0];
 
@@ -106,11 +127,11 @@ class Company {
 
   static async update(handle, data) {
     const { setCols, values } = sqlForPartialUpdate(
-        data,
-        {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url",
-        });
+      data,
+      {
+        numEmployees: "num_employees",
+        logoUrl: "logo_url",
+      });
     const handleVarIdx = "$" + (values.length + 1);
 
     const querySql = `
@@ -133,15 +154,20 @@ class Company {
 
   static async remove(handle) {
     const result = await db.query(
-        `DELETE
+      `DELETE
            FROM companies
            WHERE handle = $1
            RETURNING handle`,
-        [handle]);
+      [handle]);
     const company = result.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
   }
+
+
+  // Variable holding possible query strings which will be checked 
+  // Whatever in the query-string becomes filter for query
+  // 
 }
 
 
--- routes/companies.js
+++ routes/companies.js
@@ -47,7 +47,8 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
-  const companies = await Company.findAll();
+  const filters = req.query;
+  const companies = await Company.findAll(filters);
   return res.json({ companies });
 });
 
